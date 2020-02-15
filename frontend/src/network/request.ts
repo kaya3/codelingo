b7e8c8f00d3edde1e5019affece00dafdee819e9
wjbaker101 <willbaker10198@gmail.com>
2020-02-15 16:29:20 +0000

    Fix request for getting skills

--- frontend/src/network/request.ts
+++ frontend/src/network/request.ts
@@ -5,13 +5,11 @@ const API_URL = 'https://codelingo.werp.site/api';
 const api = axios.create({
   baseURL: API_URL,
   timeout: 10000,
-  headers: 'application/json',
+  headers: { 'Content-Type': 'application/json' },
 });
 
 export const request = {
-  getData<T = any>(uri: string, params: {} = {}): Promise<T> {
-    console.log(`Request to: ${uri}`);
-
+  getData<T = any>(uri: string, params: any): Promise<T> {
     return api.get<T>(uri, {
       params,
     }).then(response => {
