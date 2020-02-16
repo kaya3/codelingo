1f53c90689b1423295c35eb5660cbd011b44df38
Olly Pethick <olly@Ollys-MacBook-Pro.local>
2020-02-16 01:09:02 +0000

    Update login styling

--- frontend/src/components/LoginView/LoginView.tsx
+++ frontend/src/components/LoginView/LoginView.tsx
@@ -1,6 +1,7 @@
 import React, { PureComponent, ReactNode } from "react";
 import { Input, Label, Button } from "reactstrap";
 import { authClient } from "../../network/authClient";
+import { Link } from "react-router-dom";
 
 interface Props {}
 interface State {}
@@ -10,21 +11,32 @@ class LoginView extends PureComponent<Props, State> {
 
   render(): ReactNode {
     return (
-      <div className="login-view d-flex flex-column align-items-center p-2">
-        <h1>Log In</h1>
-        <Label for="username-input">Username</Label>
-        <Input id="username-input" type="text" placeholder="Alice" />
-        <p></p>
-        <Label for="password-input">Password</Label>
-        <Input id="password-input" type="password" />
-        <p></p>
-        <Button onClick={this.onClick} className="button">Log In</Button>
+      <div className="login-view d-flex flex-column w-100 justify-content-center align-items-center p-2">
+        <h1 className="font-weight-bold">codelingo</h1>
+        <hr />
+        <div className="m-2 d-flex flex-column w-100 justify-content-center align-items-center">
+          <h4>Log In</h4>
+          <Label for="username-input text-left">Username</Label>
+          <Input id="username-input" type="text" placeholder="Alice" />
+          <p></p>
+          <Label for="password-input text-left">Password</Label>
+          <Input id="password-input" type="password" />
+          <p></p>
+          <Link to="/skills">
+            <Button
+              onClick={() => this.onClick()}
+              className="button button-info"
+            >
+              Log In
+            </Button>
+          </Link>
+        </div>
       </div>
     );
   }
 
   onClick() {
-    authClient.login('alice', 'test').then(response => {
+    authClient.login("alice", "test").then(response => {
       console.log(response);
     });
   }
