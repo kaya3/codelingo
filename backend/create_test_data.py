7e6950877b9b45747cafc7ec26ca3475d20a6e10
Andrew Kay <kaya3@aston.ac.uk>
2020-02-16 09:40:56 +0000

    more lessons

--- backend/create_test_data.py
+++ backend/create_test_data.py
@@ -19,33 +19,6 @@ db.session.add_all([
 	*languages.values(),
 ])
 
-"""
-skill = Skill('Lists', language, 1)
-
-lesson1 = Lesson(skill, 1)
-#lesson2 = Lesson(skill, 2)
-
-q1 = Question(lesson1, {
-	'kind': 'blocks',
-	'text': '<p>Find the sum of the numbers in <code>nums</code>.</p>',
-	'correct': 'sum ( nums )'.split(),
-	'incorrect': 'total [ ]'.split(),
-})
-q2 = Question(lesson1, {
-	'kind': 'multiple_choice',
-	'text': '<p>What is the result of <code>sum([2, 5])</code>?</p>',
-	'correct': ['7'],
-	'incorrect': ['2', '5'], 
-})
-q3 = Question(lesson1, {
-	'kind': 'blanks',
-	'text': '<p>Complete the code to print each number from <code>nums</code>.</p>',
-	'template': 'for ### in nums:\n\t###(x)'.split('###'),
-	'correct': ['x', 'print'],
-	'incorrect': ['sum', 'output', 'nums'],
-})
-"""
-
 for lang in LANGUAGES:
 	with open('../lessons/' + lang + '.json') as f:
 		data = json.loads(f.read())
