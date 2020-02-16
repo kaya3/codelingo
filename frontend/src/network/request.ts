3cbee9a82c637a99d397b5a9af5b643c9dd02b00
wjbaker101 <willbaker10198@gmail.com>
2020-02-16 09:04:45 +0000

    Update client to point to cloud version of the api

--- frontend/src/network/request.ts
+++ frontend/src/network/request.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_URL = process.env.NODE_ENV !== 'production'
-  ? 'https://codelingo.werp.site/api'
+  ? 'https://codelingo.tech/api'
   : '/api';
 
 const api = axios.create({
